401ecb04cddddb93f060a9e945a1c63fe505c979
lucas baiba <lucasbaiba@iMac-de-lucas.local>
2022-01-20 13:41:29 +0100

    Init Front Folder with files + Sass Install

---
+++ Front/src/components/App/index.js
@@ -0,0 +1,15 @@
+/* Package imports */
+import React from 'react';
+
+/* Local imports */
+// Components
+
+const App = () => {
+  return (
+    <div className='App'>
+      RENDERING APP
+    </div>
+  )
+};
+
+export default App;
--- Front/src/index.js
+++ Front/src/index.js
@@ -1,17 +1,25 @@
+/* Package imports */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+/*  Local imports */
+// Styles
+import './styles/reset.scss';
+import './styles/index.scss';
+// Components
+import App from './components/App';
+// Store
+
+
+const rootReactElement = (
+  <App />
 );
+const target = document.getElementById('root');
+render(rootReactElement, target);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
