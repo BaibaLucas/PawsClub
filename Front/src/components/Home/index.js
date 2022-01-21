4066f82f8dbd734e19bf9528e07f92e568a6056a
lucas baiba <lucasbaiba@iMac-de-lucas.local>
2022-01-21 13:53:19 +0100

    Add baseline&illustration / Base news card

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -2,7 +2,8 @@
 import React from 'react';
 
 /* Local imports */
-import illust from '../../assets/images/paws_black_logo.png';
+import illust from '../../assets/images/pawspaper.jpg';
+import newsimg from '../../assets/images/paws_black_logo.png';
 // Components
 
 const Home = () => {
@@ -10,15 +11,47 @@ const Home = () => {
     <div className='home'>
       <div className='container'>
         <div className='container__presentation'>
-          <div className='container__presentation__image'>
-            <img src={illust} alt='illustration homepage' />
-            <div className='container__presentation__content'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at lacus mattis, consequat odio non, dictum ligula. Quisque molestie diam in tempus posuere. Nunc hendrerit tellus ac justo varius, vehicula rhoncus dui fermentum. Nullam vel nunc commodo, sollicitudin tellus eget, mattis metus.
-          </div>
-          </div>
+          <img className='container__presentation__image' src={illust} alt='illust'/>
+        <span className='container__presentation__title'> PAWS </span>
+        <div className='container__presentation__content'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum nisi tortor, porta ornare turpis placerat sit amet.
+        </div>
         </div>
         <div className='container__news'>
-          NEWS
+          <div className='container__news__title'>
+            Latest news
+          </div>
+          <div className='container__news__box'>
+            <div className='container__news__box__card'>
+              <img className='container__news__box__card__image' src={newsimg} alt='news illustration' />
+              <div className='container__news__box__card__title'>
+                Lorem ipsum dolor sit amet !
+              </div>
+              <div className='container__news__box__card__date'>
+                21 january 2022
+              </div>
+            </div>
+            <div className='container__news__box__card'>
+              <img className='container__news__box__card__image' src={newsimg} alt='news illustration' />
+              <div className='container__news__box__card__title'>
+                Lorem ipsum dolor sit amet !
+              </div>
+              <div className='container__news__box__card__date'>
+                21 january 2022
+              </div>
+            </div>
+            <div className='container__news__box__card'>
+              <img className='container__news__box__card__image' src={newsimg} alt='news illustration' />
+              <div className='container__news__box__card__title'>
+                Lorem ipsum dolor sit amet !
+              </div>
+              <div className='container__news__box__card__date'>
+                21 january 2022
+              </div>
+            </div>
+          </div>
+
+          
         </div>
         <div className='container_linesup'>
           LINESUP
