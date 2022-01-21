fbb07ba4468b7fb432d77dd3d0d425ba1d347453
lucas baiba <lucasbaiba@iMac-de-lucas.local>
2022-01-21 14:23:54 +0100

    Static HOMEPAGE / Add Stream + Lineup

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -4,6 +4,13 @@ import React from 'react';
 /* Local imports */
 import illust from '../../assets/images/pawspaper.jpg';
 import newsimg from '../../assets/images/paws_black_logo.png';
+import ow from '../../assets/images/owlogo.png';
+import lol from '../../assets/images/leagueOf.jpeg';
+import wow from '../../assets/images/shadowlandsWoW.png';
+import sylvanas from '../../assets/images/sylvanas.jpg';
+import clawlogo from '../../assets/images/clawlogo.jpg';
+import talent from '../../assets/images/talent.png';
+
 // Components
 
 const Home = () => {
@@ -51,13 +58,47 @@ const Home = () => {
             </div>
           </div>
 
-          
         </div>
         <div className='container_linesup'>
-          LINESUP
+          <div className='container__linesup__title'>
+            Lines-up
+          </div>
+          <div className='container__linesup__box'>
+            <a className='container__linesup__box__card' href='#'>
+              <img className='container__linesup__box__card__image' src={ow} alt='linesup illustration' />
+            </a>
+            <a className='container__linesup__box__card' href='#'>
+              <img className='container__linesup__box__card__image' src={lol} alt='linesup illustration' />
+            </a>
+            <a className='container__linesup__box__card' href='#'>
+              <img className='container__linesup__box__card__image' src={wow} alt='linesup illustration' />
+            </a>
+          </div>
         </div>
         <div className='container_stream'>
-          STREAM
+        <div className='container__linesup__title'>
+            Stream
+          </div>
+          <div className='container__stream__box'>
+            <a className='container__stream__box__card' href='#'>
+              <img className='container__stream__box__card__image' src={talent} alt='stream illustration' />
+              <div className='container__stream__box__card__title'>
+                Aenean interdum nisi tortor
+              </div>
+            </a>
+            <a className='container__stream__box__card' href='#'>
+              <img className='container__stream__box__card__image' src={sylvanas} alt='stream illustration' />
+              <div className='container__stream__box__card__title'>
+                Aenean interdum nisi tortor
+              </div>
+            </a>
+            <a className='container__stream__box__card' href='#'>
+              <img className='container__stream__box__card__image' src={clawlogo} alt='stream illustration' />
+              <div className='container__stream__box__card__title'>
+                Aenean interdum nisi tortor
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
