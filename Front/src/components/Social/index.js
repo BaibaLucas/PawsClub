f638c50a76bafdcdd4de9fc2986088ca9fae9a9c
BaibaLucas <69453967+BaibaLucas@users.noreply.github.com>
2022-01-23 13:14:37 +0100

    Create SocialNetwork Components

--- Front/src/components/Social/index.js
+++ Front/src/components/Social/index.js
@@ -1,5 +1,6 @@
 /* Package imports */
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 /* Local imports */
 
@@ -8,7 +9,28 @@ import React from 'react';
 const Social = () => {
   return (
     <div className='social'>
-
+      <div className='container'>
+        <div className='container__list'>
+          <div className='container__list__items'>
+          <SocialIcon network='twitter' bgColor='#F0FF00'  fgColor='#0000' url="https://twitter.com" />
+          </div>
+          <div className='container__list__items'>
+          <SocialIcon network='discord' bgColor='#F0FF00' fgColor='#0000' url="https://discord.com" />
+          </div>
+          <div className='container__list__items'>
+          <SocialIcon network='instagram' bgColor='#F0FF00' fgColor='#0000' url="https://instagram.com" />
+          </div>
+          <div className='container__list__items'>
+          <SocialIcon network='twitch' bgColor='#F0FF00' fgColor='#0000' url="https://twitch.com" />
+          </div>
+          <div className='container__list__items'>
+          <SocialIcon network='youtube' bgColor='#F0FF00' fgColor='#0000'url="https://youtube.com" />
+          </div>
+          <div className='container__list__items'>
+          <SocialIcon network='facebook' bgColor='#F0FF00' fgColor='#0000' url="https://facebook.com" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
