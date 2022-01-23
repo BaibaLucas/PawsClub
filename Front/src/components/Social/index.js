a4ba0416c586ff719e742774c9ce9fdafcd41c49
BaibaLucas <69453967+BaibaLucas@users.noreply.github.com>
2022-01-23 12:55:59 +0100

    Add react-social-icons

---
+++ Front/src/components/Social/index.js
@@ -0,0 +1,16 @@
+/* Package imports */
+import React from 'react';
+
+/* Local imports */
+
+// Components
+
+const Social = () => {
+  return (
+    <div className='social'>
+
+    </div>
+  )
+};
+
+export default Social;
