94b33c2b3985a13636cdc13e0b35174481fdbc61
Baiba Lucas <baiba.lucas@gmail.com>
2022-01-25 14:57:10 +0100

    Trying to do some test => Failed

--- Front/src/components/Header/index.js
+++ Front/src/components/Header/index.js
@@ -1,14 +1,21 @@
 /* Package imports */
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 /* Local imports */
-import logo from '../../assets/images/pawswhite.png'
+
+import logo from '../../assets/images/pawswhite.png';
+import Burger from './burger/index';
 
 
 // Components
 
 const Header = () => {
+
+  // Burger State
+  const [open, setOpen] = useState(false);
+
+
   return (
     <div className='header'>
       <div className='container'>
@@ -18,9 +25,7 @@ const Header = () => {
           </NavLink>
         </div>
         <div className='container__nav'>
-            <span />
-						<span />
-						<span />
+            <Burger open={open} setOpen={setOpen} />
         </div>
       </div>
     </div>
