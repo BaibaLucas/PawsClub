e22178bc5304d48d7b5950b2ce18dcc1868fde1d
Baiba Lucas <baiba.lucas@gmail.com>
2022-01-26 13:12:15 +0100

    Login component + styling + route StaticV1

--- Front/src/components/Login/index.js
+++ Front/src/components/Login/index.js
@@ -8,9 +8,36 @@ import React from 'react';
 const Login = ({}) => {
   return(
     <div className='login'>
-      Login
+      <div className='container'>
+        <div className='container__title'>
+          <h1>connexion</h1>
+        </div>
+        <form className='container__form'>
+          <label className='container__form__label' htmlFor='email'>Email</label>
+          <input
+          className='container__form__input'
+          type='email'
+          name='email'
+          id='email'
+          placeholder='Email'
+          // value={emailValue}
+          // onChange={onChange}
+          />
+          <label className='container__form__label' htmlFor='password'>Password</label>
+          <input
+          className='container__form__input'
+          type='password'
+          name='password'
+          id='password'
+          placeholder='Password'
+          // value={emailValue}
+          // onChange={onChange}
+          />
+          <button className='container__form__button' type='submit'>Login</button>
+        </form>
+      </div>
     </div>
   )
 };
 
-export default Login
+export default Login;
