f0563b94e403bf8b5a02b00761847e8fd8f1af80
Baiba Lucas <baiba.lucas@gmail.com>
2022-01-27 13:19:37 +0100

    Styling Linesup Component SCSS

--- Front/src/components/LinesUp/index.js
+++ Front/src/components/LinesUp/index.js
@@ -1,10 +1,12 @@
 /* Package imports */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 /* Local imports */
 import diablo3 from '../../assets/images/diablo3.png';
 import lol from '../../assets/images/leagueOf.jpeg';
 import wow from '../../assets/images/shadowlandsWoW.png';
+
 // Components
 
 const LinesUp = () => {
@@ -18,7 +20,9 @@ const LinesUp = () => {
           <div className='container__presentation__content'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue ipsum finibus, tempor neque a, imperdiet lorem. Donec interdum est consectetur magna fermentum tristique. Aliquam et dignissim leo. Donec mollis commodo sollicitudin.
           </div>
-          <div className='container__box'>
+        </div>
+        <div className='container__box'>
+          <NavLink to='#'>
             <div className='container__box__card'>
               <img className='container__box__card__image' src={wow} alt='wow illustration' />
               <div className='container__box__card__title'>
@@ -28,6 +32,8 @@ const LinesUp = () => {
                 Open
               </div>
             </div>
+          </NavLink>
+          <NavLink to='#'>
             <div className='container__box__card'>
               <img className='container__box__card__image' src={lol} alt='lol illustration' />
               <div className='container__box__card__title'>
@@ -37,6 +43,8 @@ const LinesUp = () => {
                 Open
               </div>
             </div>
+          </NavLink>
+          <NavLink to='#'>
             <div className='container__box__card'>
               <img className='container__box__card__image' src={diablo3} alt='d3 illustration' />
               <div className='container__box__card__title'>
@@ -46,7 +54,7 @@ const LinesUp = () => {
                 SOON
               </div>
             </div>
-          </div>
+          </NavLink>
         </div>
       </div>
     </div>
