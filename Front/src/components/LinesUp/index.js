6cf19c64fb00b868ccc6c5fa12c8bef798bfecc7
Baiba Lucas <baiba.lucas@gmail.com>
2022-01-27 12:41:17 +0100

    Init Files & Folders for LinesUp component / Create fake content inside component

---
+++ Front/src/components/LinesUp/index.js
@@ -0,0 +1,56 @@
+/* Package imports */
+import React from 'react';
+
+/* Local imports */
+import diablo3 from '../../assets/images/diablo3.png';
+import lol from '../../assets/images/leagueOf.jpeg';
+import wow from '../../assets/images/shadowlandsWoW.png';
+// Components
+
+const LinesUp = () => {
+  return(
+    <div className='linesup'>
+      <div className='container'>
+        <div className='container__presentation'>
+          <div className='container__presentation__title'>
+            <h1>LINES UP</h1>
+          </div>
+          <div className='container__presentation__content'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue ipsum finibus, tempor neque a, imperdiet lorem. Donec interdum est consectetur magna fermentum tristique. Aliquam et dignissim leo. Donec mollis commodo sollicitudin.
+          </div>
+          <div className='container__box'>
+            <div className='container__box__card'>
+              <img className='container__box__card__image' src={wow} alt='wow illustration' />
+              <div className='container__box__card__title'>
+                WoW
+              </div>
+              <div className='container__box__card__status'>
+                Open
+              </div>
+            </div>
+            <div className='container__box__card'>
+              <img className='container__box__card__image' src={lol} alt='lol illustration' />
+              <div className='container__box__card__title'>
+                LoL
+              </div>
+              <div className='container__box__card__status'>
+                Open
+              </div>
+            </div>
+            <div className='container__box__card'>
+              <img className='container__box__card__image' src={diablo3} alt='d3 illustration' />
+              <div className='container__box__card__title'>
+                Diablo 3
+              </div>
+              <div className='container__box__card__status'>
+                SOON
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default LinesUp;
