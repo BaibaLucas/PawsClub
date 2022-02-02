ddc9ec4d9726d60d66967f7d91c989e3709823d2
Baiba Lucas <baiba.lucas@gmail.com>
2022-02-02 12:18:03 +0100

    Newsdetails component init folders & files

---
+++ Front/src/components/Newsdetails/index.js
@@ -0,0 +1,16 @@
+/* Package imports */
+import React from 'react';
+
+/* Local imports */
+
+// Components
+
+const Newsdetails = () => {
+  return(
+    <div className='newsdetails'>
+      NEWS DETAILS
+    </div>
+  )
+};
+
+export default Newsdetails;
