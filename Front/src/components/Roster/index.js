f53f05c7180aecaf03b17ccafa12453203f899f7
Baiba Lucas <baiba.lucas@gmail.com>
2022-02-08 11:16:01 +0100

    Init files & folders Roster component

---
+++ Front/src/components/Roster/index.js
@@ -0,0 +1,16 @@
+/* Package imports */
+import React from 'react';
+
+/* Local imports */
+
+// Components
+
+const Roster = () => {
+  return(
+    <div className='roster'>
+      roster
+    </div>
+  )
+}
+
+export default Roster;
