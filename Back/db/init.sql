9e59ce5cb7f998dcf7ffae621116fe1aec9262a1
Baiba Lucas <baiba.lucas@gmail.com>
2022-02-09 12:51:00 +0100

    Insert fakeValue into DB

--- Back/db/init.sql
+++ Back/db/init.sql
@@ -71,5 +71,18 @@ CREATE TABLE "m2m_user_belong_section" (
 -- Grant privileges for user
 -------------------------------------
 GRANT ALL PRIVILEGES ON "role", "user", "tag", "section", "news", "m2m_user_belong_section" TO noway;
+-------------------------------------
+-- Insert values into tables
+-------------------------------------
+INSERT INTO "role"("name") VALUES
+('member'),
+('moderator'),
+('administrator');
+
+INSERT INTO "section"("name", "title", "description", "content") VALUES
+('World of Warcraft', 'World of Warcraft', 'Bienvenue sur la section World Of Warcraft de la communauté PAWS. Créer courant de l''été 2021, sur le serveur EU-Archimonde Alliance n''a cessé d''agrandir ses rangs. Suite à un choix nous avons décider debut 2022 de migrer la guilde côté Horde sur le serveur EU-Hyjal', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.'),
+('League Of Legends', 'League Of Legends', 'Bienvenue sur la section League of Legends de la communauté PAWS. Préparez vous pour la nouvelle campagne de Guerande !','Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.'),
+('Rocket League', 'Rocket League', 'Bienvenue sur la section Rocket League de la communauté PAWS.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.'),
+('Diablo 3', 'Diablo 3', 'Bienvenue sur la section Diablo 3 de la communauté PAWS.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.');
 
 COMMIT;
