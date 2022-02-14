dcae12d64c419584f19bed4b96ed8242d076d4d8
Baiba Lucas <baiba.lucas@gmail.com>
2022-02-14 14:57:12 +0100

    Mapper Controller Create User

--- Back/app.js
+++ Back/app.js
@@ -1,6 +1,8 @@
 /* Package required */
 const express = require('express');
 require('dotenv').config({ path: '../.env'});
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 /* Local required */
 const router = require('./app/routers/router');
@@ -8,8 +10,26 @@ const router = require('./app/routers/router');
 /* Server */
 const app = express();
 
+app.use(express.json());
+app.use(cors('*'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, UPDATE, DELETE');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
+});
+
 app.use(router);
 
-app.listen(3001, () => {
-  console.log('Server running on :', 3001);
+app.listen(process.env.PORT, () => {
+  console.log('Server running on :', process.env.PORT);
 });
---
+++ Back/app/dataMappers/userDataMapper.js
@@ -0,0 +1,19 @@
+/* Package required */
+
+/* Local required */
+const client = require('./client');
+
+/* DataMappers */
+
+module.exports = {
+
+  /* Create new user */
+  async createNewUser(newUser) {
+    console.log('newUser in Mapper', newUser);
+    const result = await client.query('INSERT INTO "user"(email, password, username) VALUES ($1, $2, $3) RETURNING *', [newUser.email, newUser.password, newUser.username]);
+    if (result.rowCount === 0) {
+      return null
+    }
+    return result.rows[0];
+  }
+}
