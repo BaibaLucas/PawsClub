be4f9d57bb30aa2da7765403f042341cb3c1c89b
Baiba Lucas <baiba.lucas@gmail.com>
2022-02-15 12:04:03 +0100

    Redux Base architecture Front

---
+++ Front/src/containers/SignUp.js
@@ -0,0 +1,19 @@
+/* Package imports */
+import { connect } from 'react-redux';
+
+/* Local imports */
+import Signup from '../components/SignUp';
+
+/* Container */
+
+// Component States (Signup)
+const mapStateToProps = (state) => ({
+
+});
+
+// Component Func (Signup)
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signup);
