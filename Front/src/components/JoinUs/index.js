b4bc3df9561550bfb05f091dd25a75e29d2cd1a1
Baiba Lucas <baiba.lucas@gmail.com>
2022-03-07 18:47:55 +0100

    Add discord widget

--- Front/src/components/JoinUs/index.js
+++ Front/src/components/JoinUs/index.js
@@ -1,24 +1,22 @@
 /* Package imports */
-import React from 'react';
+import React, { useEffect } from 'react';
+
+
 
 /* Local imports */
 
 // Components
 
-
 const JoinUs = () => {
+
   return(
     <div className='joinus'>
       <div className='container'>
         <div className='container__title'>
           <h1>JoinUs</h1>
         </div>
-        <div className='container__content'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu sem, viverra at sagittis eleifend, suscipit ut leo. Integer tincidunt in odio elementum lacinia. Integer massa elit, venenatis non ex in, gravida aliquet est.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu sem, viverra at sagittis eleifend, suscipit ut leo. Integer tincidunt in odio elementum lacinia. Integer massa elit, venenatis non ex in, gravida aliquet est.
-        </div>
         <div className='container__discord'>
-          DISCORD
+         <iframe src="https://discord.com/widget?id=844693699589570580&theme=dark" title='discord server' width="80%" height="80%" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </div>
       </div>
     </div>
