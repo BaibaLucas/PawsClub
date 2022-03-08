6baefb7a48aa67705d9f9b8f68f9e2d2cb3c47cc
Baiba Lucas <baiba.lucas@gmail.com>
2022-03-08 10:35:22 +0100

    Init files&folder + Base create news

---
+++ Front/src/components/CreateNews/index.js
@@ -0,0 +1,33 @@
+/* Package imports */
+import React from 'react';
+
+/* Local imports */
+
+// Components
+
+const CreateNews = () => {
+
+  return(
+    <div className='createnews'>
+      <div className='container'>
+        <div className='container__title'>
+         <h1> Create news </h1>
+        </div>
+      <form className='container__form'>
+        <label className='container__form__label'>
+          
+        </label>
+        <input className='container__form__input'>
+          
+        </input>
+        <button className='container__form__button'>
+          
+        </button>
+      </form>
+      </div>
+      
+    </div>
+  )
+};
+
+export default CreateNews;
---
+++ Front/src/containers/CreateNews.js
@@ -0,0 +1,22 @@
+/* Package imports */
+import { connect } from 'react-redux';
+
+/* Local imports */
+import CreateNews from '../components/CreateNews';
+
+// Action
+
+/* Container */
+
+// Component States (CreateNews)
+const mapStateToProps = (state) => ({
+  
+});
+
+// Component Func (CreateNews)
+const mapDispatchToProps = (dispatch) => ({
+
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateNews);
