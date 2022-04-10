60bccc50491a06796c45b4e1f7fe1d42506311b2
BaibaLucas <69453967+BaibaLucas@users.noreply.github.com>
2022-04-11 00:37:22 +0200

    Update init.sql

--- Back/db/init.sql
+++ Back/db/init.sql
@@ -87,24 +87,5 @@ INSERT INTO "user"("username", "email", "password", "profilurl", "stream", "role
 ('heda', 'heda@gmail.com', 'Reape2022*', '', 'https://www.twitch.tv/hedarim', '1'),
 ('liwest', 'li@gmail.com', 'Reape2022*', '', 'https://www.twitch.tv/liwesttv', '1');
 
--- INSERT INTO "section"("name", "title", "description", "content") VALUES
--- ('World of Warcraft', 'World of Warcraft', 'Bienvenue sur la section World Of Warcraft de la communauté PAWS. Créer courant de l''été 2021, sur le serveur EU-Archimonde Alliance n''a cessé d''agrandir ses rangs. Suite à un choix nous avons décider debut 2022 de migrer la guilde côté Horde sur le serveur EU-Hyjal', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.'),
--- ('League Of Legends', 'League Of Legends', 'Bienvenue sur la section League of Legends de la communauté PAWS. Préparez vous pour la nouvelle campagne de Guerande !','Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.'),
--- ('Rocket League', 'Rocket League', 'Bienvenue sur la section Rocket League de la communauté PAWS.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.'),
--- ('Diablo 3', 'Diablo 3', 'Bienvenue sur la section Diablo 3 de la communauté PAWS.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur ut massa in fermentum. Curabitur varius laoreet mauris, et scelerisque ante tempor ac. Nunc eu fringilla dolor. Duis semper eget dui sit amet tincidunt. Mauris molestie ex semper dolor finibus blandit. In eget sem turpis. Morbi sollicitudin est sit amet massa tincidunt sagittis. Nam blandit tempor semper. Nam convallis venenatis purus, eu molestie sapien vulputate consequat. Suspendisse fermentum volutpat ornare. Cras ut nisl felis. Maecenas sit amet metus egestas lacus euismod lacinia ac quis urna. Aliquam erat volutpat. Nulla elementum enim a urna feugiat pharetra.');
-
--------------------------------------
---- /!\ CAN'T WORK IF WE HAVNT CREATE SOME USERS/TAG BEFORE INSERT VALUES
--------------------------------------
--- INSERT INTO "tag"("name") VALUES
--- ('Try');
--- ('Try2');
--- INSERT INTO "news"("title", "subtitle", "content", "newsurl", "date", "time", "user_id", "tag_id") VALUES
--- ('TITLE1', 'suBTITLE1', 'Content1', 'http://myphotourlnews.fr', '02/16/2022', '13:00', '4', '1');
--- ('TITLE2', 'suBTITLE2', 'Content2', 'http://myphotourlnews.fr', '02/16/2022', '14:00', '6', '2');
--------------------------------------
---- /!\ CAN'T WORK IF WE HAVNT CREATE SOME USERS/TAG BEFORE INSERT VALUES
--------------------------------------
-
 
 COMMIT;
